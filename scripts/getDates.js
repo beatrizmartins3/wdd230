735f19e0937d5c008439e997c5b35ad470d6aa16
Beatriz Martins <biiam3462@gmail.com>
2023-09-14 16:20:12 -0300

    home page

---
+++ scripts/getDates.js
@@ -0,0 +1,11 @@
+
+// returns: Tuesday, December 16, 2017 11:09:42
+
+var year="2023";
+var thisYear=document.querySelector("#year");
+thisYear.textContent=year;
+
+
+let mod=document.lastModified;
+let theMod=document.querySelector("#lastModified");
+theMod.textContent=mod;
