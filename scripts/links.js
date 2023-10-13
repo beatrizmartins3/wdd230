c59197ed35a06d591617616c6b39b23afb1fedd8
Beatriz Martins <biiam3462@gmail.com>
2023-10-13 19:43:48 -0300

    chamber

--- scripts/links.js
+++ scripts/links.js
@@ -1,66 +1,35 @@
 const baseURL="https://beatrizmartins3.github.io/wdd230/";
 const linksURL="https://beatrizmartins3.github.io/wdd230/data/links.json";
-
 const cards=document.querySelector("#cards");
-
-async function getLinks() {
+async function getLinks(linksURL) {
     const response = await fetch(linksURL);
     const data = await response.json();
     displayLinks(data);
+    console.log(data);
   }
   
-getLinks();
+
 
 function displayLinks(weeks){
     weeks.forEach((week) => {
-        const li=document.createElement("li");
-        const ul=document.createElement("ul");
-        const sec=document.createElement("sec");
+        let fullname=document.createElement("h3");
+        let card=document.createElement("section");
+        let card2=document.createElement("ul");
+        let portrait=document.createElement("li");
+        let portrait2=document.createElement("a");
 
-        weeks.links.forEach((link) => {
-            const a=document.createElement("a");
-            a.href=link.url;
-            a.textContent=link.title;
-
-            li.appendChild(a);
-            ul.appendChild(li);
-        });
-        sec.appendChild(ul);
-        cards.appendChild(sec)
+        fullname.textContent= "Learning Activities" ;
+        portrait.textContent=`${week.week}: ${week.title}`;
+        portrait2.setAttribute("href",week.url);
+       
+        card.appendChild(fullname);
+        portrait.appendChild(portrait2)
+        card.appendChild(card2);
+        card.appendChild(portrait);
+        cards.appendChild(card);
     });
+    console.log(cards);
     
 }
+getLinks(linksURL);
 
-const currentTemp=document.querySelector('#desc');
-const weatherIcon=document.querySelector('#weather-icon');
-const captionDesc=document.querySelector('#figcaption');
-
-const url='https://api.openweathermap.org/data/2.5/weather?lat=-5.08&lon=-42.8&units=imperial&appid=57352b8a1892c6e8dcde50dc5abccec8';
-
-async function apiFetch(){
-    try{
-        const response= await fetch(url);
-        if(response.ok){
-            const data= await response.json();
-            console.log(data);
-        } else{
-            throw Error(await response.text());
-        }
-    } catch (error){
-            console.log(error);
-        }
-    }
-
-apiFetch();
-
-function displayResults(data){
-    
-    currentTemp.innerHTML=`${data.main.temp}&deg;F `;
-    const iconsrc='https://openweathermap.org/img/w/02n.png';
-    let desc=data.weather[0].description;
-    weatherIcon.setAttribute('src',iconsrc);
-    weatherIcon.setAttribute('alt','icon');
-
-    captionDesc.textContent=`${desc}`;
-
-}
